0ca95b4683083d88dd8a461662a26f6ce30a0afe
zhangxiaoyun1 <2210545262@qq.com>
2019-12-04 17:41:13 +0800

    zxy1204

---
+++ person/张晓贇/build/precache-manifest.7d66672712af4fb4430294db623bd4e4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef61f4af5d76863e1f3b01fb85ba39f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cda309b43d978ad183d",
+    "url": "/static/css/2.a6e9608c.chunk.css"
+  },
+  {
+    "revision": "242b0df1b9cf4b356b7b",
+    "url": "/static/css/main.10588155.chunk.css"
+  },
+  {
+    "revision": "4cda309b43d978ad183d",
+    "url": "/static/js/2.60fd62dc.chunk.js"
+  },
+  {
+    "revision": "242b0df1b9cf4b356b7b",
+    "url": "/static/js/main.981200c7.chunk.js"
+  },
+  {
+    "revision": "628ea25a63cbc5b89a27",
+    "url": "/static/js/runtime-main.9f147cbc.js"
+  },
+  {
+    "revision": "c7f8f2b9c80c222abbf3f7628b54e74e",
+    "url": "/static/media/15.c7f8f2b9.png"
+  },
+  {
+    "revision": "d6029b58fde3cbfcdc5420c5fa5a1345",
+    "url": "/static/media/房子.d6029b58.png"
+  }
+]);
