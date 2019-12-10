40e3817098f161a5cbb3baa12eaddd622050bed7
zhangxiaoyun1 <2210545262@qq.com>
2019-12-10 13:58:59 +0800

    zxy1210

---
+++ project/项目启动/after/build/precache-manifest.3a036ae22c578f22fca28147c907563f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3768011494b2a8abcc2df2400d97a0d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b97e222dfedfc8d80878",
+    "url": "/static/css/2.a6e9608c.chunk.css"
+  },
+  {
+    "revision": "a781382183fefebaa744",
+    "url": "/static/css/main.ab57c27b.chunk.css"
+  },
+  {
+    "revision": "b97e222dfedfc8d80878",
+    "url": "/static/js/2.44e66a45.chunk.js"
+  },
+  {
+    "revision": "a781382183fefebaa744",
+    "url": "/static/js/main.3e70c6ca.chunk.js"
+  },
+  {
+    "revision": "628ea25a63cbc5b89a27",
+    "url": "/static/js/runtime-main.9f147cbc.js"
+  }
+]);
