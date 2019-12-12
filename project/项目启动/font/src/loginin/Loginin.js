e35ac21456e28968119e8af14ae8a689853a74e0
wangyingyingwyy <2423457156@qq.com>
2019-12-12 09:28:46 +0800

    wyy

--- project/项目启动/font/src/loginin/Loginin.js
+++ project/项目启动/font/src/loginin/Loginin.js
@@ -13,7 +13,9 @@ export default class Loginin extends Component {
             warningname:'',
             warningpwd:'',
             warningpwd2:'',
-            warningphone:''
+            warningphone:'',
+            warningcode:'',
+            code:''
         }
     }
     addItem=()=>{
@@ -26,6 +28,7 @@ export default class Loginin extends Component {
         var password=document.getElementById('password').value;//密码
         var password2=document.getElementById('password2').value;//确认密码
         var phone=document.getElementById('phone').value;//手机号
+        var code=document.getElementById('code').value;
         if(iname.length === 0){
             this.setState({
                 warningname:'请输入用户名'
@@ -42,36 +45,67 @@ export default class Loginin extends Component {
             this.setState({
                 warningphone:'手机号格式错误'
             })
-        }
-        //传给后端的数据
-        let data={
-            iname:iname,//用户名
-            password:password,//密码
-            phone:phone,//手机号
-        }
-        //将对象转换为字符串传递
-        var send=JSON.stringify(data);
-        console.log(send)
-        //发送post请求
-        fetch('http://localhost:3001/api/user/reg',{
-            method: 'POST', 
-            body: send, // data can be `string` or {object}!
-            headers: new Headers({
-                'Content-Type': 'application/json'
-                })
-        })
-        .then((res)=>res.json())
-        .then((res)=>{
-            //接收响应信息，如果为true,则跳转登录页面
-            if(res.ok===true){
-                window.location.href="http://localhost:3000/#/login"
-            }else{
-                window.location.href="http://localhost:3000/#/loginin"
+        }else if(this.state.code!==code){
+            this.setState({
+                warningcode:'验证码错误'
+            })
+        }else{
+              //传给后端的数据
+            let data={
+                iname:iname,//用户名
+                password:password,//密码
+                phone:phone,//手机号
             }
-        }).catch(function(err){
-            console.log(err);
-        })
+            //将对象转换为字符串传递
+            var send=JSON.stringify(data);
+            console.log(send)
+            //发送post请求
+            fetch('http://localhost:3001/api/user/reg',{
+                method: 'POST', 
+                body: send, // data can be `string` or {object}!
+                headers: new Headers({
+                    'Content-Type': 'application/json'
+                    })
+            })
+            .then((res)=>res.json())
+            .then((res)=>{
+                //接收响应信息，如果为true,则跳转登录页面
+                if(res.ok===true){
+                    window.location.href="http://localhost:3000/#/login"
+                }else{
+                    window.location.href="http://localhost:3000/#/loginin"
+                }
+            }).catch(function(err){
+                console.log(err);
+            })
+            }
     }
+    //获取验证码
+    code=()=>{
+        this.state.code='';
+        var box = document.getElementById('box');
+        var arr = ['a', 'b', 'c', 'd', 'e', 'f', 'A', 'B', 'C', 'D', 'E', 'F', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]; //0-21
+        var strr = "qwertyuiopasdfghjklzcvnmQWERTYUIOPASDFGHKLZXCVBNM1234567890";
+        var change=()=>{
+            var str = '';
+            var str2='';
+            for (var i = 0; i < 4; i++) {
+                var index = Math.round(Math.random() * (strr.length - 1));
+                var fs = Math.round(Math.random() * (30 - 12) + 12);
+                var r = Math.round(Math.random() * 255);
+                var g = Math.round(Math.random() * 255);
+                var b = Math.round(Math.random() * 255);
+                str2=str2+strr[index];
+                str = str + "<span style='font-size:" + fs + "px;color:rgb(" + r + "," + g + "," + b + ");'>" + strr[index] + "</span>";
+            }
+            box.innerHTML = str;
+            this.setState({
+                code:str2
+            })
+        }
+        change();
+    }
+
     //页面渲染
     render() {
         return (
@@ -92,32 +126,44 @@ export default class Loginin extends Component {
                     <WingBlank>
                         <form>
                             <span> 昵 称 ：</span>
-                            <input id="iname" type='text' placeholder='请输入您的昵称' style={{backgroundColor:'antiquewhite',borderRadius:10,width:'40%',height:35,marginLeft:25}} />
+                            <input id="iname" type='text' autocomplete="off" placeholder='请输入您的昵称' style={{backgroundColor:'antiquewhite',borderRadius:10,width:'40%',height:35,marginLeft:25}} />
                             <span style={{fontSize:'14px',color:'red'}}>
                                 {this.state.warningname}
                             </span>
                             <WhiteSpace/>
                             <span> 密 码 ：</span>
-                            <input name='password' id="password"  type='password' placeholder='请输入您的密码' style={{backgroundColor:'antiquewhite',borderRadius:10,width:'40%',height:35,marginLeft:26}}/>
+                            <input name='password' autocomplete="off" id="password"  type='password' placeholder='请输入您的密码' style={{backgroundColor:'antiquewhite',borderRadius:10,width:'40%',height:35,marginLeft:26}}/>
                             <span style={{fontSize:'14px',color:'red'}}>
                                 {this.state.warningpwd}
                             </span>
                             <WhiteSpace/>
                             <span>确认密码：</span>
-                            <input  name='password2' id="password2" type='password' placeholder='请再次输入您的密码' style={{backgroundColor:'antiquewhite',borderRadius:10,width:'40%',height:35}}/>
+                            <input  name='password2' autocomplete="off" id="password2" type='password' placeholder='请再次输入您的密码' style={{backgroundColor:'antiquewhite',borderRadius:10,width:'40%',height:35}}/>
                             <span style={{fontSize:'14px',color:'red'}}>
                                 {this.state.warningpwd2}
                             </span>
                             <WhiteSpace/>
                             <span>手机号：</span>
-                            <input  name="phone" id="phone" type='text' placeholder='请输入您的手机号' style={{backgroundColor:'antiquewhite',borderRadius:10,width:'40%',height:35,marginLeft:19}}/>              
+                            <input  name="phone" autocomplete="off" id="phone" type='text' placeholder='请输入您的手机号' style={{backgroundColor:'antiquewhite',borderRadius:10,width:'40%',height:35,marginLeft:19}}/>              
                             <span style={{fontSize:'14px',color:'red'}}>
                                 {this.state.warningphone}
                             </span>
-                            <button style={{float:'right',height:35}}>获取验证码</button>
                             <WhiteSpace/>
-                            <span>验证码：</span>
-                            <input  name='code' type='text' placeholder='请输入验证码' style={{backgroundColor:'antiquewhite',borderRadius:10,width:'40%',height:35,marginLeft:19}} />
+                            {/* 验证码 */}
+                            <div style={{width:'100%',margin:'0 auto'}}>
+                                <span>验证码：</span>
+                                <input  id="code" autocomplete="off" name='code' type='text' placeholder='字母区分大小写' style={{backgroundColor:'antiquewhite',borderRadius:10,width:'40%',height:35,marginLeft:19}} />
+                                <div style={{float:'right',marginRight:'2%'}}>
+                                <div id="box" onClick={()=>this.code()}>点击获取</div>
+                                    <WhiteSpace/>
+                                    <span id='changes' onClick={()=>this.code()}>看不清？请点击刷新</span>
+                                </div>
+                                <div>
+                                    <span style={{fontSize:'14px',color:'red'}}>
+                                        {this.state.warningcode}
+                                    </span>
+                                </div>
+                            </div>
                             <WhiteSpace/>
                             <WhiteSpace />
                             <WhiteSpace />
