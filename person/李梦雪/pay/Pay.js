8930e1411826029442fb86627af367206525b99d
limengxue130402 <2503114011@qq.com>
2019-12-17 08:04:53 +0800

    p

--- person/李梦雪/pay/Pay.js
+++ person/李梦雪/pay/Pay.js
@@ -4,6 +4,20 @@ import {WhiteSpace, WingBlank,Toast,} from 'antd-mobile';
 
 export default class Pay extends Component {
 
+    constructor(props){
+        super(props);
+        this.state={
+            
+            pwd:'请输入您的密码'
+        }
+    }
+
+    
+    changePwd = (e)=>{
+        this.setState({
+            pwd:e.target.value
+        })
+    }
 
     successToast = ()=> { 
         if(this.state.pwd ==='请输入您的密码' || this.state.pwd === ''){
@@ -26,18 +40,15 @@ export default class Pay extends Component {
                         <span style={{margin:'0 auto',fontSize:25,color:'white'}}>
                             确认付款
                         </span>      
-                        {/* <Link to='/help'>
-                            <img src={require('./images/help.png')} style={{width:30,height:30,float:'right',paddingTop:10,marginRight:'30%'}}/>
-                        </Link>          */}
                     </div>
                 </div>
                 <div style={{textAlign:'center',marginTop:'20%'}}>
                     <span style={{fontSize:40}}>￥</span>
-                    <span style={{fontSize:50,fontWeight:'bold'}}>6000</span>
+                    <span style={{fontSize:50,fontWeight:'bold'}}>{this.props.match.params.price.slice(6,10)}</span>
                 </div>
                 <div style={{height:'20%',width:'100%',marginTop:'8%'}}>
                     <span style={{float:'left',marginLeft:'5%'}}>支付宝账号</span>
-                    <span style={{float:'right',marginRight:'5%'}}>12345678910</span>
+                    <span style={{float:'right',marginRight:'5%'}}>{this.props.match.params.price.slice(17)}</span>
                 </div>
                 <br/>
                 <hr style={{marginTop:'3%'}}/>
@@ -58,7 +69,7 @@ export default class Pay extends Component {
                 <div style={{height:'20%',width:'100%',marginTop:'3%'}}>
                     <span style={{float:'left',marginLeft:'5%'}}>输入密码</span>
                     <span style={{float:'right'}}>
-                        <input style={{width:'80%'}} type='password' placeholder='请输入密码'/>
+                        <input onChange={this.changePwd} style={{width:'80%'}} type='password' value={this.state.pwd}/>
                     </span>
                 </div>
 
