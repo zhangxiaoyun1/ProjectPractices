1cde1e9ed75a4f24e6e7e7f2dba2d39723e41ecf
zhangxiaoyun1 <2210545262@qq.com>
2019-12-17 09:58:36 +0800

    rot

--- project/项目启动/后台/HomeExpress/routes/house.js
+++ project/项目启动/后台/HomeExpress/routes/house.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const pg = require('pg');
 const pgdb = require('../config/dbconfig');
+const path = require('path');
+const fs = require('fs');
 //地理位置定位
 const https = require('https');
 //连接数据库
@@ -9,39 +11,81 @@ const con = new pg.Pool(pgdb);
 con.connect();
 
 //添加房屋信息
-router.post('/house', function (req, res) {
-  var homeid = (new Date()).valueOf();
-  var realname = req.body.realname;
-  var phone = req.body.phone;
-  var price = req.body.price;
-  var city = req.body.city;
-  var address = req.body.address;
-  var type = req.body.type;
-  var hometype = req.body.hometype;
-  var floor = req.body.floor;
-  var face = req.body.face;
-  var lift = req.body.lift;
-  var wifi = req.body.wifi;
-  var heating = req.body.heating;
-  var conditioner = req.body.conditioner;
-  var homeimage = req.body.homeimage;
-  var pushtime = new Date().toLocaleDateString();
+router.post('/house',function(req,res){
+  var userid=req.body.userid;
+  var homeid=(new Date()).valueOf();
+  var realname=req.body.realname;
+  var phone=req.body.phone;
+  var price=req.body.price;
+  var city=req.body.city;
+  var detail=req.body.detail;
+  var apname=req.body.apname;
+  var address=req.body.address;
+  var type=req.body.type;
+  var hometype=req.body.hometype;
+  var floor=req.body.floor;
+  var face=req.body.face;
+  var lift=req.body.lift;
+  var wifi=req.body.wifi;
+  var heating=req.body.heating;
+  var conditioner=req.body.conditioner;
+  var files=req.body.files;
+  var imgs=[];
+  for(var i=0;i<files.length;i++){
+    var name;
+    var data = files[i].url.split(',');
+    var buf = Buffer.from(data[1], "base64");
+    if (data[0].indexOf('png') || data[0].indexOf('PNG')) {
+        name = `${homeid}-${i}.png`;
+    } else if (data[0].indexOf('jpg') || data[0].indexOf('JPG') || data[0].indexOf('jpeg') || data[0].indexOf('JPEG')) {
+        name = `${homeid}-${i}.jpg`;
+    } else if (data[0].indexOf('gif') || data[0].indexOf('GIF')) {
+         name = `${homeid}-${i}.gif`;
+    }
+    var paths=path.join(__dirname,'../../images/houseimage/', name);
+    imgs.push(name);
+    fs.writeFileSync(paths, buf, { "encoding": "base64" });
+   }
+  var homeimage=imgs.join(',');
+  var pushtime=new Date().toLocaleDateString();
   con.query(`insert into homemessage(homeid,realname,phone,price,
-        city,address,type,hometype,floor,face,lift,wifi,heating,conditioner,homeimage,pushtime) 
-        values($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16)`,
-    [homeid, realname, phone, price, city, address, type, hometype, floor, face, lift,
-      wifi, heating, conditioner, homeimage, pushtime], function (err, result) {
-        if (err) {
+      city,address,type,hometype,floor,face,lift,wifi,heating,detail,apname,conditioner,homeimage,pushtime) 
+      values($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18)`,
+      [homeid,realname,phone,price,city,address,type,hometype,floor,face,lift,
+          wifi,heating,detail,apname,conditioner,homeimage,pushtime],function(err,result){
+    if(err){
+      console.log(err);
+    }else{
+      con.query(`update usermessage set ishome=true
+      where userid=$1`,[userid],function(err,result){
+        if(err){
           console.log(err);
-        } else {
-
-          res.send({ ok: true, msg: '发布成功！' });
+        }else{
+           res.send({ok:true,msg:'发布成功！'});
         }
-      })
+      });
+    }
+  })
 })
-/**
- * 获得所有房源信息列表
- */
+//读取房屋图片
+router.get('/housess/:img',function(req,res){
+  var img=req.params.img;
+  var imgPath=path.join(__dirname,'../../images/houseimage/', img);
+  fs.readFile(imgPath, (err, data) => {
+    if (err) {
+        console.log(err);
+    } else {
+        if (img.indexOf('png') >0 || img.indexOf('PNG')>0) {
+            res.writeHead(200, { "Content-Type": "image/png" });
+        } else if (img.indexOf('jpg')>0 || img.indexOf('JPG')>0 || img.indexOf('jpeg') || img.indexOf('JPEG')) {
+            res.writeHead(200, { "Content-Type": "image/jpeg" });                            
+        } else if (img.indexOf('gif')>0|| img.indexOf('GIF')>0) {
+            res.writeHead(200, { "Content-Type": "image/gif" });                            
+        }
+        res.end(data);
+    }
+})
+})
 router.get('/house',function(req,res){
   con.query("select * from homemessage",function(err,result){
     if(err){
@@ -51,41 +95,27 @@ router.get('/house',function(req,res){
     }
   })
 })
-
-/**
- * 详情页
- */
-
-//获得房屋详细信息用于详情页 
-router.get('/houses/:homeid',function(req,res){
-  console.log(req.params.homeid);
-  con.query("select * from homemessage where homeid=$1",[req.params.homeid],function(err,result){
-    if(err){
+//详情页
+router.get('/houses/:homeid', function (req, res) {
+  con.query("select * from homemessage where homeid=$1", [req.params.homeid], function (err, result) {
+    if (err) {
       console.log(err);
-    }else{
-      res.send({ok:true,msg:result.rows});
+    } else {
+      res.send({ ok: true, msg: result.rows });
     }
   })
 })
-//访问百度地图用来展示房源位置
-router.get('/map/:homeid',function(req,res){
-  console.log(req.params.homeid);
-  con.query("select * from homemessage where homeid=$1",[req.params.homeid],function(err,result){
-    if(err){
+//查找住房信息
+router.get('/house', function (req, res) {
+  con.query("select * from homemessage", function (err, result) {
+    if (err) {
       console.log(err);
-    }else{
-      res.send({ok:true,msg:result.rows});
+    } else {
+      res.send({ ok: true, msg: result.rows });
     }
   })
 })
-
-/**
- * 心愿单
- */
-
-//查找住房信息用于心愿单
 router.get('/house/:id', function (req, res) {
-  console.log(JSON.parse(req.params.id))
   var homeMessage = JSON.parse(req.params.id);
   var homeUserid = homeMessage.userName;
   var homeidList = [];
@@ -114,12 +144,8 @@ router.get('/house/:id', function (req, res) {
           } else {
             res.send({ ok: 'true', msg0: homeMessageList, msg1: homeidList, msg2: homeDreamFlagList })
           }
-
-          //console.log(result.rows);
-          //console.log(homeDreamFlagList);
         }
       })
-      //console.log(homeDreamFlagList,homeMessageList,homeidList);
     }
   })
 
@@ -132,15 +158,12 @@ router.get('/getDream/:id', function (req, res) {
     if (err) {
       console.log(err)
     } else {
-      //console.log(result.rows);
       var homeList = [];
       var homeIdList = [];
       for (var i = 0; i < result.rows.length; i++) {
-        // console.log(result.rows[i].dreamflag)
         if (result.rows[i].dreamflag === true) {
           var dreamHomeid = result.rows[i].homeid;
           homeIdList.push(dreamHomeid);
-          //console.log(homeIdList.length);
           con.query("select * from homemessage where homeid =$1", [dreamHomeid], (err, result) => {
             if (err) {
               console.log(err);
@@ -150,20 +173,70 @@ router.get('/getDream/:id', function (req, res) {
                 if (homeList.length == homeIdList.length) {
                   res.send(homeList)
                 }
-                //console.log(homeList);
               }
-              // homeList.push(result.rows[0]);
-              // if (homeList.length == homeCount) {
-              //   res.send(homeList)
-              // }
-              // console.log(result.rows);
             }
-
           })
         }
-
       }
-
+    }
+  })
+})
+//获取租房百科所有信息
+router.get('/rentwiki',function(req,res){
+  con.query("select * from rentwiki order by rentid",function(err,result){
+      if(err){
+        res.header('Access-Control-Allow-Origin', '*');
+        console.log(err);
+      }else{
+        // console.log(result.rows);
+        res.header('Access-Control-Allow-Origin', '*');
+        res.send({ok:true,msg:result.rows});
+      }
+  })
+})
+//房屋订单详情信息
+router.post("/trade", function (req, res) {
+  var userid=req.body.userid;
+  var tradeid = (new Date()).valueOf();
+  var rentername = req.body.rentername;
+  var renterphone = req.body.renterphone;
+  var checkin = req.body.checkin;
+  var longtime = req.body.longtime;
+  var price = req.body.price;
+  var pushtime = new Date().toLocaleDateString();
+  con.query(`insert into trademanagermessage(tradeid,rentername,renterphone,checkin,longtime,price,pushtime) 
+    values($1,$2,$3,$4,$5,$6,$7)`,
+    [tradeid, rentername, renterphone, checkin,longtime, price, pushtime], function (err, result) {
+      if (err) {
+        console.log(err);
+      } else {
+	con.query(`update usermessage set isseller=true
+     	 where userid=$1`,[userid],function(err,result){
+        if(err){
+          console.log(err);
+        }else{
+           res.send({ok:true,msg:'提交成功！'});
+       }
+      })
+    }
+   })
+})
+router.get('/trade/:tradeid',function(req,res){
+  con.query("select * from trademanagermessage where tradeid=$1",[req.params.tradeid],function(err,result){
+    if(err){
+      console.log(err);
+    }else{
+      console.log(result.rows);
+      res.send({ok:true,msg:result.rows[0]});
+    }
+  })
+})
+router.get('/trade',function(req,res){
+  con.query("select * from trademanagermessage",function(err,result){
+    if(err){
+      console.log(err);
+    }else{
+      res.send({ok:true,msg:result.rows});
     }
   })
 })
@@ -177,14 +250,46 @@ router.post('/addDream', (req, res) => {
   var addDreamHomeId = addMessage.homeid;
   var dreamUser = addMessage.dreamUser;
   var dreamFlag = true;
-  con.query("insert into dreammessage(dreamid,userid,homeid,dreamFlag) values ($1,$2,$3,$4)",
-    [addDreamId, dreamUser, addDreamHomeId, dreamFlag], (err, result) => {
-      if (err) {
-        console.log(err)
+  var homeidList = [];
+  con.query("select * from dreammessage where userid = $1", [dreamUser], (err, result) => {
+    if (err) {
+      console.log(err)
+    } else {
+      if (result.rows.length === 0) {
+        con.query("insert into dreammessage(dreamid,userid,homeid,dreamFlag) values ($1,$2,$3,$4)",
+          [addDreamId, dreamUser, addDreamHomeId, dreamFlag], (err, result) => {
+            if (err) {
+              console.log(err)
+            } else {
+              res.send({ ok: 'true', msg: '添加成功' });
+            }
+          })
       } else {
-        res.send({ ok: 'true', msg: '添加成功' });
+        for(var i = 0;i<result.rows.length;i++){
+          homeidList.push(result.rows[i].homeid);
+        }
+        if(homeidList.indexOf(addDreamHomeId) !== -1){
+          con.query("update dreammessage set dreamflag = $1 where homeid =$2",[dreamFlag,addDreamHomeId],(err,result)=>{
+            if(err){
+              console.log(err)
+            }else{
+              res.send({ok:true,msg:'修改成功!'})
+            }
+          })
+        }else{
+          con.query("insert into dreammessage(dreamid,userid,homeid,dreamFlag) values ($1,$2,$3,$4)",
+          [addDreamId, dreamUser, addDreamHomeId, dreamFlag], (err, result) => {
+            if (err) {
+              console.log(err)
+            } else {
+              res.send({ ok: 'true', msg: '添加成功' });
+            }
+          })
+        }
       }
-    })
+
+    }
+  })
 })
 //删除心愿单
 router.get("/deleteDream", (req, res) => {
@@ -192,7 +297,6 @@ router.get("/deleteDream", (req, res) => {
 })
 router.post("/deleteDream", (req, res) => {
   var deleteMessage = req.body;
-  // var deletedreamId = deleteMessage.dreamid;
   var deleteDreamHomeid = deleteMessage.homeid;
   var deleteDreamUserid = deleteMessage.dreamUser;
   var deleteDreamFlag = false;
@@ -205,49 +309,27 @@ router.post("/deleteDream", (req, res) => {
   }
   )
 })
-//获取租房百科所有信息
-router.get('/rentwiki', function (req, res) {
-  con.query("select * from rentwiki", function (err, result) {
-    if (err) {
-      res.header('Access-Control-Allow-Origin', '*');
+router.get('/houses/:homeid',function(req,res){
+  console.log(req.params.homeid);
+  con.query("select * from homemessage where homeid=$1",[req.params.homeid],function(err,result){
+    if(err){
       console.log(err);
-    } else {
-      console.log(result.rows);
-      res.header('Access-Control-Allow-Origin', '*');
-      res.send({ ok: true, msg: result.rows });
+    }else{
+      res.send({ok:true,msg:result.rows});
     }
   })
 })
-//房屋订单详情信息
-router.get('/trade', function (req, res) {
-  res.render('trade');
+//访问百度地图
+router.get('/map/:homeid',function(req,res){
+  console.log(req.params.homeid);
+  con.query("select * from homemessage where homeid=$1",[req.params.homeid],function(err,result){
+    if(err){
+      console.log(err);
+    }else{
+      res.send({ok:true,msg:result.rows});
+    }
+  })
 })
-/**
- * 添加租房订单
- */
-router.post("/trade", function (req, res) {
-  var tradeid = (new Date()).valueOf();
-  var realname = req.body.realname;
-  var phone = req.body.phone;
-  var longtime = req.body.longtime;
-  var price = req.body.price;
-  var pushtime = new Date().toLocaleDateString();
-  con.query(`insert into trademanagermessage(tradeid,realname,phone,longtime,price,pushtime) 
-    values($1,$2,$3,$4,$5,$6)`,
-    [tradeid, realname, phone, longtime, price, pushtime], function (err, result) {
-      if (err) {
-        console.log(err);
-      } else {
-        res.send({ ok: true, msg: '提交成功！' });
-      }
-    })
-})
-
-
-/**
- * 首页地理位置定位
- */
-
 //接受地理定位
 router.get('/getLocation',(req,res)=>{
   let apiUrl = "https://api.map.baidu.com/location/ip?ak=DxehXP3OwiGt3bRZGkajTMx6v2AeItkm&coor=bd09l"
@@ -267,9 +349,33 @@ router.get('/getLocation',(req,res)=>{
     })
   })
 })
+//搜索小区名称
+router.get('/homeSearch/:id', (req, res) => {
+  var searchMessage = JSON.parse(req.params.id);
+  var search_apartment = searchMessage.apartment;
+  con.query("select * from homemessage where apname = $1", [search_apartment], (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send({ ok: true, msg: result.rows });
+    }
+  })
+})
+//搜索城市
+router.get('/citySearch/:id', (req, res) => {
+  var citySearchMessage = JSON.parse(req.params.id);
+  var city = citySearchMessage.city;
+  con.query("select * from homemessage where city = $1", [city], (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send({ ok: true, msg: result.rows });
+    }
+  })
 
+})
 // 关注
-router.post('/notice',function(req,res){
+router.post('/noclick',function(req,res){
   var tag = req.body.tag +1;
   con.query('update rentwiki set tag=$1 where rentid=$2',[tag,req.body.rentid],function(err,result){
     if(err){
@@ -280,5 +386,16 @@ router.post('/notice',function(req,res){
     }
   })
 })
-
+router.post('/click',function(req,res){
+  console.log(req.body.click);
+  console.log(req.body.rentid);
+  con.query('update rentwiki set clickcount=$1 where rentid=$2',[req.body.click,req.body.rentid],function(err,result){
+    if(err){
+      console.log(err);
+    }
+    else{
+      res.send({ ok: 'true', msg: '成功!' });
+    }
+  })
+})
 module.exports = router;
--- project/项目启动/后台/HomeExpress/routes/user.js
+++ project/项目启动/后台/HomeExpress/routes/user.js
@@ -2,145 +2,151 @@ const express = require('express');
 var app = express();
 const router = express.Router();
 const pg = require('pg');
-const pgdb=require('../config/dbconfig')
-const fs=require('fs');
-const path=require('path');
+const pgdb = require('../config/dbconfig')
+const fs = require('fs');
+const path = require('path');
 //连接数据库
-const con=new pg.Pool(pgdb);
+const con = new pg.Pool(pgdb);
 con.connect();
 //注册
-router.post('/user/reg',function(req,res){
-  var userid=(new Date()).valueOf();
-  var iname=req.body.iname;
-  var password=req.body.password;
-  var phone=req.body.phone;
-  var userimg='user.png';
-  var settime=new Date().toLocaleDateString();
-  con.query("select * from usermessage where iname=$1",[iname],function(err,result){
-    console.log(result);
-    if(err){
+router.post('/user/reg', function (req, res) {
+  var userid = (new Date()).valueOf();
+  var iname = req.body.iname;
+  var password = req.body.password;
+  var phone = req.body.phone;
+  var userimg = 'user.png';
+  var settime = new Date().toLocaleDateString();
+  con.query("select * from usermessage where iname=$1", [iname], function (err, result) {
+    if (err) {
       console.log(err);
-    }else{
-      if(result.rows.length!==0){
-        res.send({ok:false,msg:'用户名存在'});
-      }else{
-        con.query("insert into usermessage (userid,iname,password,phone,settime,userimage) values($1,$2,$3,$4,$5,$6)",[userid,iname,password,phone,settime,userimg],function(err,result){
-          if(err){
+    } else {
+      if (result.rows.length !== 0) {
+        res.send({ ok: false, msg: '用户名存在' });
+      } else {
+        con.query("insert into usermessage (userid,iname,password,phone,settime,userimage) values($1,$2,$3,$4,$5,$6)", [userid, iname, password, phone, settime, userimg], function (err, result) {
+          if (err) {
             console.log(err);
-          }else{
-              res.send({ok:true,msg:'注册成功'});
-            }
+          } else {
+            res.send({ ok: true, msg: '注册成功' });
+          }
         })
       }
     }
   })
 })
-  
+
 //登录
-router.post('/user/login',function(req,res){
-  console.log(1);
-  var phone=req.body.phone;
-  var password=req.body.password;
-  con.query("select * from usermessage where phone=$1 and password=$2",[phone,password],function(err,result){
-    if(err){
+router.post('/user/login', function (req, res) {
+  var phone = req.body.phone;
+  var password = req.body.password;
+  con.query("select * from usermessage where phone=$1 and password=$2", [phone, password], function (err, result) {
+    if (err) {
       console.log(err);
-    }else{
-      if(result.rows.length===0){
-        res.send({ok:false,msg:'登录失败'});
+    } else {
+      if (result.rows.length === 0) {
+        res.send({ ok: false, msg: '登录失败' });
       }
-      else{
-        useridNum=result.rows[0].userid;
-        console.log(useridNum);
-        res.send({ok:true,msg:result.rows[0]});
+      else {
+        useridNum = result.rows[0].userid;
+        res.send({ ok: true, msg: result.rows[0] });
       }
     }
   })
 })
-router.post('/user/real',function(req,res,next){
-  var realname=req.body.realname;
-  var phone=req.body.phone;
-  var id=req.body.idcard;
-  var sex=req.body.sex;
-  con.query("select * from usermessage where phone=$1",[phone],function(err,result){
-    if(err){
+router.post('/user/real', function (req, res, next) {
+  var realname = req.body.realname;
+  var phone = req.body.phone;
+  var id = req.body.idcard;
+  var sex = req.body.sex;
+  con.query("select * from usermessage where phone=$1", [phone], function (err, result) {
+    if (err) {
       console.log(err);
-    }else{
-      if(result.rows.length!==0){
-        if(result.rows[0].realname!==null){
-          res.send({ok:false,msg:'已经实名认证'})
-        }else{
+    } else {
+      if (result.rows.length !== 0) {
+        if (result.rows[0].realname !== null) {
+          res.send({ ok: false, msg: '已经实名认证' })
+        } else {
           con.query(`update usermessage set realname=$1,id=$2,sex=$3
-          where phone=$4`,[realname,id,sex,phone],function(err,result){
-            if(err){
+          where phone=$4`, [realname, id, sex, phone], function (err, result) {
+            if (err) {
               console.log(err);
-            }else{
-              res.send({ok:true,msg:'认证成功'});
-            }
+            } else {
+              con.query("select * from usermessage where phone=$1", [phone], function (err, result) {
+                if (err) {
+                  console.log(err);
+                } else {
+                  res.send({ ok: true, msg: result.rows[0] });
+                }
+              })
+            
+          }
           })
-        }
-      }else{
-        res.send({ok:false,msg:'没有此用户'})
-      }
+}
+      }else {
+    res.send({ ok: false, msg: '没有此用户' })
+  }
     }
   })
 })
 //忘记密码
-router.get('/user/forget',function(req,res,next){
+router.get('/user/forget', function (req, res, next) {
   res.render('forget');
 });
-router.post('/user/forget',function(req,res,next){
-  var phone=req.body.phone;
-  var password=req.body.password;
+router.post('/user/forget', function (req, res, next) {
+  var phone = req.body.phone;
+  var password = req.body.password;
   con.query(`update usermessage set password=$1
-  where phone=$2`,[password,phone],function(err,result){
-    if(err){
+  where phone=$2`, [password, phone], function (err, result) {
+    if (err) {
       console.log(err);
-    }else{
-        res.send({ok:true,msg:'修改成功'});
+    } else {
+      res.send({ ok: true, msg: '修改成功' });
     }
   })
 })
-router.post('/user/upimgs',function(req,res,next){
-      var file=req.body.file;
-      var userid=req.body.userid;
-      var name;
-      var data = file.split(',');
-      var buf = Buffer.from(data[1], "base64");
-      if (data[0].indexOf('png') || data[0].indexOf('PNG')) {
-          name = `${userid}.png`;
-      } else if (data[0].indexOf('jpg') || data[0].indexOf('JPG') || data[0].indexOf('jpeg') || data[0].indexOf('JPEG')) {
-          name = `${userid}.jpg`;
-      } else if (data[0].indexOf('gif') || data[0].indexOf('GIF')) {
-           name = `${userid}.gif`;
-      }
-      var paths=path.join(__dirname,'../../images/usersimg/', name);
-      var userimage=name;
-      fs.writeFileSync(paths, buf, { "encoding": "base64" });
-      con.query(`update usermessage set userimage=$1
-        where userid=$2`,[userimage,userid],function(err,result){
-          if(err){
-            console.log(err);
-          }else{
-             res.send({ok:true,msg:'发布成功！'});
-          }
-        });
+//上传头像
+router.post('/user/upimgs', function (req, res, next) {
+  var file = req.body.file;
+  var userid = req.body.userid;
+  var name;
+  var data = file.split(',');
+  var buf = Buffer.from(data[1], "base64");
+  if (data[0].indexOf('png') || data[0].indexOf('PNG')) {
+    name = `${userid}.png`;
+  } else if (data[0].indexOf('jpg') || data[0].indexOf('JPG') || data[0].indexOf('jpeg') || data[0].indexOf('JPEG')) {
+    name = `${userid}.jpg`;
+  } else if (data[0].indexOf('gif') || data[0].indexOf('GIF')) {
+    name = `${userid}.gif`;
+  }
+  var paths = path.join(__dirname, '../../images/usersimg/', name);
+  var userimage = name;
+  fs.writeFileSync(paths, buf, { "encoding": "base64" });
+  con.query(`update usermessage set userimage=$1
+        where userid=$2`, [userimage, userid], function (err, result) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send({ ok: true, msg: '发布成功！' });
+    }
+  });
 })
-router.get('/user/:img',function(req,res){
-  var img=req.params.img;
-  var imgPath=path.join(__dirname,'../../images/usersimg/', img);
+//读取图像
+router.get('/user/:img', function (req, res) {
+  var img = req.params.img;
+  var imgPath = path.join(__dirname, '../../images/usersimg/', img);
   fs.readFile(imgPath, (err, data) => {
     if (err) {
-        console.log(err);
+      console.log(err);
     } else {
-        if (img.indexOf('png') >0 || img.indexOf('PNG')>0) {
-            res.writeHead(200, { "Content-Type": "image/png" });
-        } else if (img.indexOf('jpg')>0 || img.indexOf('JPG')>0 || img.indexOf('jpeg') || img.indexOf('JPEG')) {
-            res.writeHead(200, { "Content-Type": "image/jpeg" });                            
-        } else if (img.indexOf('gif')>0|| img.indexOf('GIF')>0) {
-            res.writeHead(200, { "Content-Type": "image/gif" });                            
-        }
-        res.end(data);
+      if (img.indexOf('png') > 0 || img.indexOf('PNG') > 0) {
+        res.writeHead(200, { "Content-Type": "image/png" });
+      } else if (img.indexOf('jpg') > 0 || img.indexOf('JPG') > 0 || img.indexOf('jpeg') || img.indexOf('JPEG')) {
+        res.writeHead(200, { "Content-Type": "image/jpeg" });
+      } else if (img.indexOf('gif') > 0 || img.indexOf('GIF') > 0) {
+        res.writeHead(200, { "Content-Type": "image/gif" });
+      }
+      res.end(data);
     }
-})
+  })
 })
 module.exports = router;
